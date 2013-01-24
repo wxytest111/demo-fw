9dba2729505493b2a63b115ee4d0d5486536cf34
smuc <smuc@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-01-24 16:56:16 +0000

    removing unused getter from Account
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@89 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/src/main/java/com/thoughtworks/twu/model/Account.java
+++ trunk/src/main/java/com/thoughtworks/twu/model/Account.java
@@ -22,10 +22,7 @@ public class Account {
     @NotNull
     private Integer enabled;
 
-    public Integer getEnabled() {
-        return enabled;
-    }
-
+    
     public void setEnabled(Integer enabled) {
         this.enabled = enabled;
     }
