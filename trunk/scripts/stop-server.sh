bf13e3184223d4f51f2bfbf739445d9f7b771369
smuc <smuc@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-02-09 09:01:45 +0000

    fixed name issue in stop-server and added restart task
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@144 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/scripts/stop-server.sh
+++ trunk/scripts/stop-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PID=$(ps aux | grep java | grep com.trailblazers.freeriders.FreeWheelersServer | grep -v grep | awk '{ print $2 }')
+PID=$(ps aux | grep java | grep com.trailblazers.freewheelers.FreeWheelersServer | grep -v grep | awk '{ print $2 }')
 echo "Killing process with PID [${PID}]"
 kill -9 $PID
