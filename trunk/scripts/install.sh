f53fc59e6a6d5d005a61b70317f7f8f6d627f6e6
smuc <smuc@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-02-10 11:54:21 +0000

    fixing command hanging because of nohup. this should deploy successfully to ci-env
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@152 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/scripts/install.sh
+++ trunk/scripts/install.sh
@@ -21,6 +21,10 @@ ssh ${USER}@${HOST} /bin/bash << EOF
 TIMESTAMP=\$(date +"%Y-%m-%d-%HH%MM%Ss")
 mkdir -p /tmp/\$TIMESTAMP
 mv /tmp/freewheelers.zip /tmp/\$TIMESTAMP
-cd /tmp/\$TIMESTAMP && unzip freewheelers.zip
+cd /tmp/\$TIMESTAMP 
+unzip freewheelers.zip
+sh stop-server.sh
+sh db/create.sh
+nohup sh start-server.sh > server.out 2> server.err < /dev/null
 EOF
 
