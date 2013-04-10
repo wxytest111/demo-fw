42564ecc11578ec5bd3cd9682cd74e0be6f315d3
mfait <mfait@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-04-10 04:20:52 +0000

    use item mapper service in all controllers
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@202 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/src/main/java/com/trailblazers/freewheelers/web/ReserveController.java
+++ trunk/src/main/java/com/trailblazers/freewheelers/web/ReserveController.java
@@ -7,6 +7,7 @@ import com.trailblazers.freewheelers.service.AccountService;
 import com.trailblazers.freewheelers.service.ItemService;
 import com.trailblazers.freewheelers.service.ReserveOrderService;
 import com.trailblazers.freewheelers.service.impl.AccountServiceImpl;
+import com.trailblazers.freewheelers.service.impl.ItemServiceMapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,9 +24,7 @@ public class ReserveController {
 
     static final String URL = "/reserve";
 
-    @Autowired
-    ItemService itemService;
-
+    ItemService itemService = new ItemServiceMapperImpl();
     AccountService accountService = new AccountServiceImpl();
 
     @Autowired
