4ddfe6950d8925607a02635d1cd3e149f86f7fb9
mfait <mfait@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-05-03 04:41:52 +0000

    deleted unused create account in data access
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@207 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/src/main/java/com/trailblazers/freewheelers/persistence/DataAccess.java
+++ trunk/src/main/java/com/trailblazers/freewheelers/persistence/DataAccess.java
@@ -30,20 +30,4 @@ public class DataAccess {
         return names;
     }
 
-    public void createAccount(String email, String password, String name, String phoneNumber) throws SQLException, IOException {
-        Connection connection = connectionProvider.getConnection();
-
-        String sql = "insert into account (email_address, account_name, password, phone_number, enabled) ";
-        sql +=       "values ('" + email + "', '" + name + "', '" + password + "', '" + phoneNumber + "', true)";
-
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.execute();
-
-        sql  = "insert into account_role (account_name, role) values ('" + name + "', 'ROLE_USER')";
-
-        preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.execute();
-
-        connection.close();
-    }
 }
