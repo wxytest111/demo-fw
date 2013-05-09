8e29650f0e8e71a8f51d9cea6c6694957204f85a
mfait <mfait@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-05-09 09:33:09 +0000

    deleted all annotations from item model
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@221 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/src/main/java/com/trailblazers/freewheelers/model/Item.java
+++ trunk/src/main/java/com/trailblazers/freewheelers/model/Item.java
@@ -7,28 +7,13 @@ import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "item")
 public class Item {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long itemId;
-
-    @NotEmpty(message = "Please enter Item Name")
     private String name;
-
-    @DecimalMax("99999")
-    @NotNull
     private BigDecimal price;
-
-    @NotNull
     private String description;
-
-    @NotEmpty(message = "Please select Item Type")
     private String type;
-
-    @NotNull
     private Long quantity;
 
     public Long getItemId() {
