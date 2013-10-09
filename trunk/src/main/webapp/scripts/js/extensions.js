48ef2bd24cff18321a9853b6300df5a742fe7dbb
ddornell <ddornell@843859fd-cf44-46d8-9a24-7d91b6ff617c>
2013-10-09 21:22:53 +0000

    ooopsie, hardcoded form for serializeObject function
    git-svn-id: http://svn.university.thoughtworks.com/repo/twu@300 843859fd-cf44-46d8-9a24-7d91b6ff617c

--- trunk/src/main/webapp/scripts/js/extensions.js
+++ trunk/src/main/webapp/scripts/js/extensions.js
@@ -1,9 +1,5 @@
 function serializeObject(formData) {
     var data = {};
-
-    $(document.forms["addItem"]).serializeArray().forEach(
-        function (e) { data[e.name] = e.value; }
-    );
-
+    $(formData).serializeArray().forEach(function (e) { data[e.name] = e.value; });
     return data;
 }
