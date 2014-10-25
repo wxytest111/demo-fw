27acb72879e7174a3ca4769638b10533f6ca6562
davenkin <davenkin@gmail.com>
2014-10-25 14:34:36 +0530

    Cloud:server.log

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#printf "\n" | java -cp "lib/main/*:bin/main/" -XX:MaxPermSize=256M com.trailblazers.freewheelers.FreeWheelersServer > server.log &
-
-printf "\n" | java -XX:MaxPermSize=256M -jar jetty-runner-8.1.14.v20131031.jar --port 9090 --log freewheelers.log freewheelers.war > jetty.server &
+printf "\n" | java -XX:MaxPermSize=256M -jar jetty-runner-8.1.14.v20131031.jar --port 9090 --log freewheelers.log freewheelers.war > server.log &
