9132a55c0c2b8afd387a97993a93da3306f33d95
davenkin <davenkin@gmail.com>
2014-10-25 13:50:06 +0530

    Cloud:deploy script

--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PID=$(ps aux | grep java | grep com.trailblazers.freewheelers.FreeWheelersServer | grep -v grep | awk '{ print $2 }')
+PID=$(ps aux | grep java | grep jetty-runner | grep -v grep | awk '{ print $2 }')
 echo "Killing process with PID [${PID}]"
 kill -9 $PID
