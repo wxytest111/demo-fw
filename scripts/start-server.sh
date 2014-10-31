3cec67928bbed9e71a9b9b98a9af100037d3d64e
davenkin <davenkin@gmail.com>
2014-10-31 11:21:42 +0530

    Cloud:bring back

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "\n" | java -XX:MaxPermSize=256M -jar jetty-runner-8.1.14.v20131031.jar --port 8081 --log freewheelers.log freewheelers.war > server.log &
+printf "\n" | java -XX:MaxPermSize=256M -jar jetty-runner-8.1.14.v20131031.jar --port 8080 --log freewheelers.log freewheelers.war > server.log &
